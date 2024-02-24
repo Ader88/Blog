3507b301dd6d1b16fd2505709062a01796f74f75
Ader88 <151433723+Ader88@users.noreply.github.com>
2024-02-24 23:00:09 +0100

    Update routes.py - single function for creating and editing entries

--- blog/routes.py
+++ blog/routes.py
@@ -4,6 +4,15 @@ from blog.models import Entry, db
 from blog.forms import EntryForm
 from utils import generate_entries
 
+def save_entry(form, entry=None):
+    if entry is None:
+        entry = Entry()
+    form.populate_obj(entry)
+    db.session.add(entry)
+    db.session.commit()
+    flash('Wpis został zaktualizowany pomyślnie!', 'success' if entry.id else 'success')
+    return entry
+
 @app.route("/")
 def index():
     all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
@@ -15,39 +24,16 @@ def generate_blog_entries():
     return "Wpisy zostały wygenerowane."
 
 @app.route("/new-post/", methods=["GET", "POST"])
-def create_entry():
-    form = EntryForm()
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            entry = Entry(
-                title=form.title.data,
-                body=form.body.data,
-                is_published=form.is_published.data
-            )
-            db.session.add(entry)
-            db.session.commit()
-            flash('Wpis został dodany pomyślnie!', 'success')
-            return redirect(url_for('index'))
-        else:
-            for field, errors in form.errors.items():
-                for error in errors:
-                    flash(f'Błąd w polu {getattr(form, field).label.text}: {error}', 'danger')
-    return render_template("entry_form.html", form=form)
-
-
-
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
-def edit_entry(entry_id):
-    entry = Entry.query.filter_by(id=entry_id).first_or_404()
+def edit_or_create_entry(entry_id=None):
+    entry = Entry.query.get_or_404(entry_id) if entry_id else None
     form = EntryForm(obj=entry)
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            form.populate_obj(entry)
-            db.session.commit()
-            flash('Wpis został zaktualizowany pomyślnie!', 'success')
-            return redirect(url_for('index'))
-        else:
-            for field, errors in form.errors.items():
-                for error in errors:
-                    flash(f'Błąd w polu {getattr(form, field).label.text}: {error}', 'danger')
+    if form.validate_on_submit():
+        save_entry(form, entry)
+        return redirect(url_for('index'))
+    else:
+        for field, errors in form.errors.items():
+            for error in errors:
+                flash(f'Błąd w polu {getattr(form, field).label.text}: {error}', 'danger')
     return render_template("entry_form.html", form=form)
+    
