aa553654e725eabfe127c38b85667d19bc04ccf1
Ader88 <151433723+Ader88@users.noreply.github.com>
2024-02-25 13:33:59 +0100

    Update routes.py

--- blog/routes.py
+++ blog/routes.py
@@ -3,7 +3,7 @@ from blog import app
 from blog.models import Entry, db
 from blog.forms import EntryForm, LoginForm
 from utils import generate_entries
-from flask_login import login_required
+from flask_login import login_required, current_user
 
 @app.route("/")
 def index():
@@ -25,20 +25,24 @@ def save_entry(form, entry=None):
     return entry
 
 @app.route("/new-post/", methods=["GET", "POST"])
+@login_required
+def new_post():
+    form = EntryForm()
+    if form.validate_on_submit():
+        entry = save_entry(form)
+        return redirect(url_for('index'))
+    return render_template("entry_form.html", form=form)
+    
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
 @login_required
-def edit_or_create_entry(entry_id=None):
-    entry = Entry.query.get_or_404(entry_id) if entry_id else None
+def edit_post(entry_id):
+    entry = Entry.query.get_or_404(entry_id)
     form = EntryForm(obj=entry)
     if form.validate_on_submit():
         save_entry(form, entry)
         return redirect(url_for('index'))
-    else:
-        for field, errors in form.errors.items():
-            for error in errors:
-                flash(f'Błąd w polu {getattr(form, field).label.text}: {error}', 'danger')
     return render_template("entry_form.html", form=form)
-
+    
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
@@ -49,7 +53,9 @@ def login():
             session['logged_in'] = True
             session.permanent = True  # Use cookie to store session.
             flash('You are now logged in.', 'success')
-            return redirect(next_url or url_for('index'))
+            if next_url:
+                return redirect(next_url) 
+            return redirect (url_for('index'))
         else:
             errors = form.errors
     return render_template("login_form.html", form=form, errors=errors)
