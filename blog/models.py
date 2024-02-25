b0fa9b9217c08eab3ca5543995d72bfa066dd5bc
Ader88 <151433723+Ader88@users.noreply.github.com>
2024-02-25 15:58:15 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,9 @@
 from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
@@ -15,6 +16,10 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+    def is_active(self):
+        # Użytkownik jest aktywny, jeśli nie ma żadnych ograniczeń na jego konto
+        return True
 
 @login_manager.user_loader
 def load_user(user_id):
